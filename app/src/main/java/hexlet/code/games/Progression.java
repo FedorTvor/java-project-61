cdc8ddd2ce493d29490d4ee1719174f79724852b
FedorTvor <fedor_tvorogov@mail.ru>
2024-01-21 19:06:32 +0300

    change in progression

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -10,36 +10,56 @@ public class Progression {
         Scanner scanner = new Scanner(System.in);
         var finalCount = 2;
         var condition = "What number is missing in the progression?";
-
         for (var count = 0; count <= finalCount; count++) {
-            int[] massivProgression = randomMassiv(5, Engine.getRandomNumber1(), Engine.getRandomNumber1());
-            var correct = massivProgression[2];
-            var question = showMassiv(massivProgression);
+            var step = Engine.getRandomNumber1();
+            var firstNum = Engine.getRandomNumber1();
+            var massivLength = massivLength(5, 10);
+            int[] massivProgression = randomMassiv(5, step, firstNum);
+            var correct = massivProgression[hiddenElement()];
+            var question = showMassiv(massivProgression, correct);
             Engine.stepGame(condition, question, correct, count, finalCount);
         }
     }
     public static int[] randomMassiv(int massiveLength, int step, int firstNum) {
         int[] result = new int[massiveLength];
         result[0] = firstNum;
-        if (result[0] != result[1] && result[0] != 0) {
+        if (result[0] != result[1] && result[0] != 0 && result.length != 0) {
             for (var i = 1; i < result.length; i++) {
-                result[i] = result[i - 1] + step;
+                if (result[i - 1] != result[i]) {
+                    result[i] = result[i - 1] + step;
+                }
             }
-        } else {
-            result[0] = Engine.getRandomNumber1();
         }
         return result;
     }
-    public static String showMassiv(int[] massivProgression) {
-        var result = massivProgression[0] + " ";
-        var middle = 2;
+    public static String showMassiv(int[] massivProgression, int hidden) {
+        var result = massivProgression[0] + "";
+        var middle = hidden;
         for (var i = 1; i < massivProgression.length; i++) {
-            if (i != middle) {
-                result = result + massivProgression[i] + " ";
+            if (massivProgression[i] != middle) {
+                result = result + " " + massivProgression[i];
             } else {
-                result = result + " .. ";
+                result = result + " " + "..";
             }
         }
         return  result;
+    }
+    public static int hiddenElement() {
+        var hiddenElement = Engine.getRandomNumber1();
+        if (hiddenElement >= 1 && hiddenElement < 5) {
+            return hiddenElement;
+        } else {
+            hiddenElement = 2;
+        }
+        return hiddenElement;
+    }
+    public static int massivLength(int a, int b) {
+        var massivLength = Engine.getRandomNumber1();
+        if (massivLength >= a && massivLength <= b) {
+            return massivLength;
+        } else {
+            massivLength = a;
+        }
+        return massivLength;
     }
 }
